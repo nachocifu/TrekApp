a71e03da15907c02be3c6d4d86227c8fd16f3f72
Nacho <ignacio.cifuentes948@gmail.com>
2015-05-26 17:51:19 -0300

    d

--- src/services/UserService.java
+++ src/services/UserService.java
@@ -1,5 +1,20 @@
 package services;
 
+import domain.Profile;
+
 public class UserService extends AbstractService<Profile> {
+
+	@Override
+	public Profile getById(Integer whoIs, Integer id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Profile save(Integer whoIs, Profile obj) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
 	
 }
