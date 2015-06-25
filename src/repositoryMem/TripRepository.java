8b6420252e17da6afbf9902fbcf0fc95fd3ad97a
Nacho <ignacio.cifuentes948@gmail.com>
2015-06-25 02:24:06 -0300

    Limpio lo de base de datos de sql de Application. El paquete que quede para que santi vea que si trabajamos en eso. Quedo funcionando pero con bugs.

--- src/repositoryMem/TripRepository.java
+++ src/repositoryMem/TripRepository.java
@@ -8,22 +8,22 @@ import domain.Trip;
 
 public class TripRepository extends AbstractRepository<Trip> {
 
-    public TripRepository(String pathToDataBase, Class<Trip> reposClass) {
+    public TripRepository() {
     }
 
     public Collection<Trip> getTripBy(Date startDate, Date endDate, String startCity, String endCity, String description ){
-    	Collection<Trip> result = new HashSet<Trip>();
-    	for (Trip trip : this.repository) {
-			if(trip.getStartDate().before(startDate)
-					&& trip.getEndDate().after(endDate)
-					&& trip.getOriginCity().trim().contains(startCity.trim())
-					&& trip.getEndCity().trim().contains(endCity.trim())
-					&& trip.getTripDescription().trim().contains(description.trim())){
-				result.add(trip);
-			}
-		}
-    	return result;
-    	
+        Collection<Trip> result = new HashSet<Trip>();
+        for (Trip trip : this.repository) {
+            if(trip.getStartDate().before(startDate)
+                    && trip.getEndDate().after(endDate)
+                    && trip.getOriginCity().trim().contains(startCity.trim())
+                    && trip.getEndCity().trim().contains(endCity.trim())
+                    && trip.getTripDescription().trim().contains(description.trim())){
+                result.add(trip);
+            }
+        }
+        return result;
+
     }
 
 }
