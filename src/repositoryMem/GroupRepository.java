ca8457962f3533057b4735fb51bf451be9ade889
matuteale <mbuscagl@itba.edu.ar>
2015-06-25 00:53:28 -0300

    minimo cambio en repository para testear

--- src/repositoryMem/GroupRepository.java
+++ src/repositoryMem/GroupRepository.java
@@ -21,7 +21,7 @@ public class GroupRepository extends AbstractRepository<Group> {
     	add(new Group("grupo1", new Profile("naty2", "Ignacio", "Cifu", new Date(7, 5, 1994), true, "agua", "Baires", "naty.navas2@gmail.com"), 5, 3, "baires"));
     	add(new Group("grupo1", new Profile("naty2", "Ignacio", "Cifu", new Date(7, 5, 1994), true, "agua", "Baires", "naty.navas2@gmail.com"), 5, 3, "baires"));
     	add(new Group("grupo1", new Profile("naty5", "Ignacio", "Cifu", new Date(7, 5, 1994), true, "agua", "Baires", "naty.navas2@gmail.com"), 5, 3, "baires"));
-    	Group g = new Group("grupo1", new Profile("naty263", "Ignacio", "Cifu", new Date(7, 5, 1994), true, "agua", "Baires", "naty.navas2@gmail.com"), 5, 3, "baires");
+    	Group g = new Group("grupo1", new Profile("naty", "Ignacio", "Cifu", new Date(7, 5, 1994), true, "agua", "Baires", "naty.navas2@gmail.com"), 5, 3, "baires");
     	try {
 			g.addGroupTrip(new Trip(new Date(7, 5, 2020), new Date(7, 5, 2023), 500000.0, "hola soy jesus", "buenos aires", "cordoba"));
 		} catch (InvalidPermissionException e) {
